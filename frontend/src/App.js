0ebf556b7e23d8667297e6f4e5863326700304b8
JiaYouYUI <118436304+JiaYouYUI@users.noreply.github.com>
2025-01-25 19:24:18 +0800

    Finish Integrate Frontend and Backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-        <div>Physiotherapy</div>
+        <Routes>
+          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/" element={<LoginPage/>} />
+          <Route path="*" element={<LoginPage/>} />
+        </Routes>
     </div>
   );
 }
